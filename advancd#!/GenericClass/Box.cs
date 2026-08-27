9223260b93409723b68ce0d52058044ed92527b6
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2026-08-27 16:48:00 +0300

    Add project files.

---
+++ advancd#!/GenericClass/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace advancd__.GenericClass
+{
+    internal class Box<T>
+    {
+        public T Value { get; set; }
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+    }
+}
---
+++ advancd#!/Program.cs
@@ -0,0 +1,20 @@
+﻿using advancd__.GenericClass;
+
+namespace advancd__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region Normal Generic
+            //Box<int> BoxOfElements = new Box<int>(value: 5);
+
+            //Console.WriteLine($"Value in Box: {BoxOfElements.Value}"); 
+            //#endregion
+
+
+
+
+        }
+    }
+}
